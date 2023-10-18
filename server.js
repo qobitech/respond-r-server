2940c998ae8d123c7e1b5850f6113a740f536227
Frank Qobi <qobitek@gmail.com>
2023-10-18 14:39:23 +0100

    .

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express")
-const Stream = require("node-rtsp-stream")
+// const Stream = require("node-rtsp-stream")
+const ffmpeg = require("fluent-ffmpeg")
 const cors = require("cors")
 
 const app = express()
@@ -15,22 +16,41 @@ app.use(
 
 app.get("/stream", (req, res) => {
   const newRtspStreamUrl = req.query.rtsp
-  let currentRtspStreamUrl = ""
+  // let currentRtspStreamUrl = ""
 
-  // Create the WebSocket stream only if it doesn't exist or the RTSP URL has changed
-  if (!stream || currentRtspStreamUrl !== newRtspStreamUrl) {
-    if (stream || newRtspStreamUrl === "stop") {
-      stream.stop()
-    }
-    stream = new Stream({
-      name: "Camera Stream",
-      streamUrl: newRtspStreamUrl,
-      wsPort: 9999,
+  // // Create the WebSocket stream only if it doesn't exist or the RTSP URL has changed
+  // if (!stream || currentRtspStreamUrl !== newRtspStreamUrl) {
+  //   if (stream || newRtspStreamUrl === "stop") {
+  //     stream.stop()
+  //   }
+  //   stream = new Stream({
+  //     name: "Camera Stream",
+  //     streamUrl: newRtspStreamUrl,
+  //     wsPort: 9999,
+  //   })
+  //   currentRtspStreamUrl = newRtspStreamUrl
+  // }
+
+  // res.sendStatus(200).json({ url: `ws://localhost:9999` })
+  const ffmpegCommand = ffmpeg(newRtspStreamUrl)
+    .inputFormat("rtsp")
+    .inputOptions("-rtsp_transport", "tcp")
+    .videoCodec("copy")
+    .audioCodec("aac")
+    // .outputOptions([
+    //   "-hls_time 10", // Set segment duration
+    //   "-hls_list_size 6", // Set playlist size
+    // ])
+    // .output(`${hlsOutputDir}/output.m3u8`)
+    .on("end", () => {
+      console.log("Conversion finished.")
     })
-    currentRtspStreamUrl = newRtspStreamUrl
-  }
-
-  res.send(200).json({ url: `ws://localhost:9999` })
+    .on("error", (err) => {
+      console.error("Error:", err)
+    })
+    .writeToStream(res, function (retcode, error) {
+      console.log("file has been converted succesfully")
+    })
 })
 
 app.listen(port, () => {
