5b40a429899e7c142d09223f55ba62aff2a54b7f
Frank Qobi <qobitek@gmail.com>
2023-10-18 15:28:57 +0100

    done

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const express = require("express")
-// const Stream = require("node-rtsp-stream")
-const ffmpeg = require("fluent-ffmpeg")
+const Stream = require("node-rtsp-stream")
 const cors = require("cors")
-const pathToFfmpeg = require("ffmpeg-static")
-ffmpeg.setFfmpegPath(pathToFfmpeg)
 
 const app = express()
 const port = 3002
@@ -18,41 +15,22 @@ app.use(
 
 app.get("/stream", (req, res) => {
   const newRtspStreamUrl = req.query.rtsp
-  // let currentRtspStreamUrl = ""
+  let currentRtspStreamUrl = ""
 
-  // // Create the WebSocket stream only if it doesn't exist or the RTSP URL has changed
-  // if (!stream || currentRtspStreamUrl !== newRtspStreamUrl) {
-  //   if (stream || newRtspStreamUrl === "stop") {
-  //     stream.stop()
-  //   }
-  //   stream = new Stream({
-  //     name: "Camera Stream",
-  //     streamUrl: newRtspStreamUrl,
-  //     wsPort: 9999,
-  //   })
-  //   currentRtspStreamUrl = newRtspStreamUrl
-  // }
+  // Create the WebSocket stream only if it doesn't exist or the RTSP URL has changed
+  if (!stream || currentRtspStreamUrl !== newRtspStreamUrl) {
+    if (stream || newRtspStreamUrl === "stop") {
+      stream.stop()
+    }
+    stream = new Stream({
+      name: "Camera Stream",
+      streamUrl: newRtspStreamUrl,
+      wsPort: 9999,
+    })
+    currentRtspStreamUrl = newRtspStreamUrl
+  }
 
-  // res.sendStatus(200).json({ url: `ws://localhost:9999` })
-  const ffmpegCommand = ffmpeg(newRtspStreamUrl)
-    .inputFormat("rtsp")
-    .inputOptions("-rtsp_transport", "tcp")
-    .videoCodec("copy")
-    .audioCodec("aac")
-    // .outputOptions([
-    //   "-hls_time 10", // Set segment duration
-    //   "-hls_list_size 6", // Set playlist size
-    // ])
-    // .output(`${hlsOutputDir}/output.m3u8`)
-    .on("end", () => {
-      console.log("Conversion finished.")
-    })
-    .on("error", (err) => {
-      console.error("Error:", err)
-    })
-    .writeToStream(res, function (retcode, error) {
-      console.log("file has been converted succesfully")
-    })
+  res.sendStatus(200).json({ url: `ws://localhost:9999` })
 })
 
 app.listen(port, () => {
